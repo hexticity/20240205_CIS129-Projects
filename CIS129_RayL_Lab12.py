4a3cd676fcc6861df8fc73096303f7276253eaaf
hexticity <159094903+hexticity@users.noreply.github.com>
2024-04-29 20:57:08 -0700

    Update CIS129_RayL_Lab12.py

--- CIS129_RayL_Lab12.py
+++ CIS129_RayL_Lab12.py
@@ -1,69 +1,57 @@
-// main module
-Module main()
-// Declare input variables
-  	Declare String inputName, String inputType, Integer inputAge
+# Create a class named Pet to store information about pets
+class Pet:
+    # Set up the initial properties of the Pet when we make a new one
+    def __init__(self, name='', type='', age=0):
+        self.name = name  # Name of the pet
+        self.type = type  # What kind of animal (like Dog, Cat)
+        self.age = age    # How old the pet is
 
-// class variable to hold a pet
-Declare Pet Animal
- 
-// create a Pet object
-Set Animal = new Pet()
- 
-// Get values for a pet
-Display “Enter a pet name:”
-Input inputName
-Animal.setName(inputName)
- 
-Display “Enter a pet type:”
-Input inputType
-Animal.setType(inputType)
- 
-Display “Enter a pet age:”
-Input inputAge
-Animal.setAge(inputAge)
- 
-// Show values for this pet
-Display “The pet name is “,Animal.getName()
-Display “The pet type is “,Animal.getType()
-Display “The pet age is “,Animal.getAge()
-End Module
- 
- 
-Class Pet
-  	// Fields
-  	Private String name
-  	Private String type
-  	Private Integer age
- 
-  	// Constructor
-  	Public Module Pet(String n, String t, Integer a)
-        	Set name = n
-        	Set type = t
-        	Set age = a
-  	End Module
- 
-  	// Mutators
-  	Public Module setName(String n)
-        	Set name = n
-  	End Module
- 
-  	Public Module setType(String t)
-        	Set type = t
-  	End Module
- 
-Public Module setAge(Integer a)
-        	Set age = a
-  	End Module
- 
-  	// Accessors
-  	Public Function String getName()
-        	Return name
-  	End Function
- 
-  	Public Function String getType()
-        	Return type
-  	End Function
- 
-  	Public Function getAge()
-        	Return age
-  	End Function
+    # A function to change the pet's name
+    def set_name(self, name):
+        self.name = name
+
+    # A function to change the type of the pet
+    def set_type(self, type):
+        self.type = type
+
+    # A function to change how old the pet is
+    def set_age(self, age):
+        self.age = age
+
+    # A function to get the pet's name when we need it
+    def get_name(self):
+        return self.name
+
+    # A function to get what type of animal the pet is
+    def get_type(self):
+        return self.type
+
+    # A function to find out how old the pet is
+    def get_age(self):
+        return self.age
+
+# This is where we put the code that runs when we start the program
+def main():
+    # Ask the user to type in a name and save it
+    input_name = input("Enter a pet name: ")
+    # Ask the user to type in what type of pet it is and save it
+    input_type = input("Enter a pet type: ")
+    # Ask the user to type in the pet's age, make sure it's a number, and save it
+    input_age = int(input("Enter a pet age: "))
+
+    # Make a new pet object from the Pet class
+    animal = Pet()
+
+    # Set the details of the pet using what the user typed in
+    animal.set_name(input_name)
+    animal.set_type(input_type)
+    animal.set_age(input_age)
+
+    # Print out the details about the pet
+    print(f"The pet name is {animal.get_name()}")
+    print(f"The pet type is {animal.get_type()}")
+    print(f"The pet age is {animal.get_age()}")
+
+# This line makes sure the code in main() runs only if this file is the main program
+if __name__ == "__main__":
+    main()
